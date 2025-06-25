4ec5daf511cfbf7896919afc24418a2c1da25711
Rice <126218083+keenwarice@users.noreply.github.com>
2025-06-24 19:30:07 -0700

    implemented detecting phrase after wake word is detected

--- stt/transcriber.py
+++ stt/transcriber.py
@@ -1,10 +1,14 @@
 import sounddevice as sd
+import soundfile as sf
 import queue
 import json
+import time
+import numpy as np
 from vosk import Model, KaldiRecognizer
 
 model_path = "models/vosk-model-small-en-us-0.15"
 SR = 16000
+CHANNELS = 1
 q = queue.Queue() # i was a bit confused with the spelling, skull
 
 def audio_callback(indata,frames, time,status):
@@ -12,16 +16,22 @@ def audio_callback(indata,frames, time,status):
         print(f"UR CODE IS COOKED!!!! ALERT!! {status}")
     q.put(bytes(indata))
 
-def recognize_from_microphone():
+def recognize_from_microphone(on_detect_callback):
     model=Model(model_path)
     recognizer = KaldiRecognizer(model, SR)
-
+    filename=f"recorded_{int(time.time())}.wav"
+    audio_frames=[]
     print("speak now you lowly mortal")
     with sd.RawInputStream(samplerate=SR, blocksize=8000, dtype="int16",channels=1,callback=audio_callback):
         while True:
             data=q.get()
+            audio_frames.append(data)
             if recognizer.AcceptWaveform(data):
                 result=json.loads(recognizer.Result())
                 text= result.get("text","")
                 if text:
+                    all_audio= b"".join(audio_frames)
+                    with sf.SoundFile(filename, mode="x",samplerate=SR, channels=CHANNELS, subtype="PCM_16") as file:
+                        file.write(np.frombuffer(all_audio, dtype="int16"))
+                    on_detect_callback(filename, text)
                     print(f"you have said: {text}")
