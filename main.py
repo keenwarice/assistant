bfbd1793d436111e8be8b5455d8eca709ac4c452
Rice <126218083+keenwarice@users.noreply.github.com>
2025-06-24 19:29:17 -0700

    implemented wake word and phrase detection

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-'''from audio.mic_input import rec_sound
-rec_sound()'''
+from audio.wake_word import detect_wake_word
 from stt.transcriber import recognize_from_microphone
-recognize_from_microphone()
+
+
+def after():
+    print("WAKE WORD DETECTED")
+    recognize_from_microphone(send_phrase)
+
+def send_phrase(filename, phrase):
+    print("phrase recieved")
+    print(phrase)
+
+if __name__ == "__main__":
+    detect_wake_word(after) #had to do this because python didnt let me run it
