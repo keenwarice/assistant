758b04bcbdfb3b13fba6781123c30b1227a7adf2
Rice <126218083+keenwarice@users.noreply.github.com>
2025-06-24 21:12:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from audio.wake_word import detect_wake_word
 from stt.transcriber import recognize_from_microphone
-
+from ai.recognition import recognize_user
 
 def after():
     print("WAKE WORD DETECTED")
@@ -9,6 +9,11 @@ def after():
 def send_phrase(filename, phrase):
     print("phrase recieved")
     print(phrase)
+    name, score= recognize_user(filename)
+    print("the user is "+name)
+    
+
+
 
 if __name__ == "__main__":
     detect_wake_word(after) #had to do this because python didnt let me run it
