8c59ba93b05135b3bf9a604ff7a74fd0f531476e
Rice <126218083+keenwarice@users.noreply.github.com>
2025-06-24 19:32:00 -0700

    fixed an issue with circular import

--- audio/wake_word.py
+++ audio/wake_word.py
@@ -4,6 +4,7 @@ import queue
 import json
 from vosk import Model, KaldiRecognizer
 
+
 model_path = "models/vosk-model-small-en-us-0.15"
 SR = 16000
 q = queue.Queue()
@@ -13,12 +14,7 @@ def audio_callback(indata,frames, time,status):
         print(f"UR CODE IS COOKED!!!! ALERT!! {status}")
     q.put(bytes(indata))
 
-def after():
-    print("yippie wake word was said")
-    # add voice recognition
-    # add ai after voice recognition
-
-def recognize_from_microphone():
+def detect_wake_word(on_detect_callback):
     model=Model(model_path)
     wake_words= ["sirius", "serious", "seriously"] # just in case
     recognizer = KaldiRecognizer(model, SR)
@@ -34,12 +30,8 @@ def recognize_from_microphone():
             if partial_text:
                 last_word=partial_text.split()[-1]
                 if last_word in wake_words and last_word != last_detected:
-                    after()
+                    on_detect_callback()
                     last_detected = last_word
                 elif last_word != last_detected:
                     last_detected=""
                 print('the human has spoken the word "' + last_word+'"' )
-
-if __name__ == "__main__":
-    # prolly gonna run this from another file anyways but whatever
-    recognize_from_microphone()
