59d0d73f38e670f303e963274818660de6c4c9ad
Rice <126218083+keenwarice@users.noreply.github.com>
2025-08-07 18:19:53 -0700

    fixed stale queue bug

--- stt/transcriber.py
+++ stt/transcriber.py
@@ -9,15 +9,19 @@ from vosk import Model, KaldiRecognizer
 model_path = "models/vosk-model-small-en-us-0.15"
 SR = 16000
 CHANNELS = 1
-q = queue.Queue() # i was a bit confused with the spelling, skull
+q = queue.Queue()
 
 def audio_callback(indata,frames, time,status):
     if status:
         print(f"UR CODE IS COOKED!!!! ALERT!! {status}")
-    q.put(bytes(indata))
+    if hear:
+        q.put(bytes(indata))
 
 def recognize_from_microphone(on_detect_callback):
+    global hear
+    hear=True
     model=Model(model_path)
+    q.queue.clear()
     recognizer = KaldiRecognizer(model, SR)
     filename=f"recorded_{int(time.time())}.wav"
     audio_frames=[]
@@ -25,9 +29,10 @@ def recognize_from_microphone(on_detect_callback):
     start_time=time.time()
     speech_heard=False
     with sd.RawInputStream(samplerate=SR, blocksize=8000, dtype="int16",channels=1,callback=audio_callback):
-        while True:
+        while hear:
             if time.time() - start_time > 5 and not speech_heard: #prevent accidental spying
                 print("nvm")
+                hear=False
                 return
             data=q.get()
             audio_frames.append(data)
@@ -41,6 +46,7 @@ def recognize_from_microphone(on_detect_callback):
                     all_audio= b"".join(audio_frames)
                     with sf.SoundFile(filename, mode="x",samplerate=SR, channels=CHANNELS, subtype="PCM_16") as file:
                         file.write(np.frombuffer(all_audio, dtype="int16"))
+                    hear=False
                     on_detect_callback(filename, text)
                     print(f"you have said: {text}")
-                    break
+                    return
