c733de673bbe4567a23b640726040afbcb04806c
Rice <126218083+keenwarice@users.noreply.github.com>
2025-08-07 18:20:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from audio.wake_word import detect_wake_word
 from stt.transcriber import recognize_from_microphone
 from ai.recognition import recognize_user
 from ai.ai_response import respond
+import pyttsx3
+engine = pyttsx3.init() 
+engine.setProperty('rate', 175)
+engine.setProperty('volume',1.0) 
+engine.setProperty('voice', engine.getProperty('voices')[14].id) # CHANGE THIS TO THE BEST ONE FOR YOUR OS
+def start():
+    while True:
+        detect_wake_word(after)
 
 def after():
     print("WAKE WORD DETECTED")
@@ -15,8 +23,12 @@ def send_phrase(filename, phrase):
         os.remove(filename)
         print("privacy secured! "+filename+" is gone!")
     except Exception as e:
-        print("PRIVACY MAY BE IN RISK: "+e)
-    print(respond(phrase,name))
-    
+        print("PRIVACY MAY BE IN RISK: ",e)
+    response=respond(phrase,name)
+    print(response)
+    engine.say(response)
+    engine.runAndWait()
+    engine.stop()
+
 if __name__ == "__main__":
-    detect_wake_word(after) #had to do this because python didnt let me run it
+    start()
